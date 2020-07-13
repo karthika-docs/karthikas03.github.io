b6012b12d21798470b407df33c8788647a71e4ed
karthika subramani <ks54471@uga.edu>
2020-07-13 12:50:36 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ importScripts('./scripts/analytics-sw.js');
 
 self.analytics.trackingId = 'UA-77119321-2';
 
-self.addEventListener('push', function (event) {
+self.addEventListener('push', async function (event) {
   console.log('Received push');
   var notificationTitle = 'Hello';
   var notificationOptions = {
@@ -24,6 +24,15 @@ self.addEventListener('push', function (event) {
     var dataText = event.data.text();
     notificationTitle = 'Received Payload';
     notificationOptions.body = 'Push data: \'' + dataText + '\'';
+  }
+  const notifications = await self.registration.getNotifications();
+  let currentNotification;
+  console.log(notifications)
+  for(let i = 0; i < notifications.length; i++) {
+    currentNotification = notifications[i];
+    console.log(i)
+    console.log(currentNotification)
+    
   }
  
   event.waitUntil(Promise.all([self.registration.showNotification(notificationTitle, notificationOptions), self.analytics.trackEvent('push-received')]));
