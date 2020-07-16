8f93329c22a8d468410b40fbe186c9b633c2c90f
karthika subramani <ks54471@uga.edu>
2020-07-16 00:44:58 -0400

    Update service-worker_demo2.js

--- service-worker_demo2.js
+++ service-worker_demo2.js
@@ -27,6 +27,14 @@ async function listNotifications(notificationTitle, notificationOptions ){
 //   return Promise.resolve()
 }
 
+self.addEventListener('install', function(event) {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting();
+
+  // Perform any other actions required for your
+  // service worker to install, potentially inside
+  // of event.waitUntil();
+});
 
 self.addEventListener('push', async function (event) {
   console.log('Received push');
