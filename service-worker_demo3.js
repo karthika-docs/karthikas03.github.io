e44a9a69a0adca8ac4a455ad32ef9b75bd6c4ebc
karthika subramani <ks54471@uga.edu>
2020-08-13 09:53:47 -0400

    Update service-worker_demo3.js

--- service-worker_demo3.js
+++ service-worker_demo3.js
@@ -29,7 +29,7 @@ async function listNotifications(notificationTitle, notificationOptions ){
 
 function slowFunction()
 {
-	var baseNumber = 50
+	var baseNumber = 20
 	console.log('computation started')
 	console.time('mySlowFunction');
 	let result = 0;	
