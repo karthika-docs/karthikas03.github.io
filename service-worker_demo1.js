bdc2f3104180620ee39c7a39a30510c87491cd02
karthika subramani <ks54471@uga.edu>
2020-09-09 12:12:30 -0400

    Update service-worker_demo1.js

--- service-worker_demo1.js
+++ service-worker_demo1.js
@@ -9,9 +9,22 @@ self.analytics.trackingId = 'UA-77119321-2';
 
 async function listNotifications(notificationTitle, notificationOptions ){
   
-   self.registration.showNotification(notificationTitle, notificationOptions).then(async() => {
-        // Resolve promise AFTER the notification is displayed
-        const notifications = await self.registration.getNotifications();
+//    self.registration.showNotification(notificationTitle, notificationOptions).then(async() => {
+//         // Resolve promise AFTER the notification is displayed
+//         const notifications = await self.registration.getNotifications();
+//         let currentNotification;
+//         console.log(notifications)
+//         for(let i = 0; i < notifications.length; i++) {
+//           currentNotification = notifications[i];
+//           console.log(i)
+//           console.log(currentNotification) 
+//           // Remember to close the old notification.
+//           currentNotification.close();
+//         }
+//         return Promise.resolve();
+//     });
+  
+  const notifications = await self.registration.getNotifications();
         let currentNotification;
         console.log(notifications)
         for(let i = 0; i < notifications.length; i++) {
@@ -21,10 +34,8 @@ async function listNotifications(notificationTitle, notificationOptions ){
           // Remember to close the old notification.
           currentNotification.close();
         }
-        return Promise.resolve();
-    });
      
-//   return Promise.resolve()
+  return Promise.resolve()
 }
 
 self.addEventListener('install', function(event) {
@@ -38,28 +49,28 @@ self.addEventListener('activate', function(event) {
   console.log('activated');
 });
 
-// self.addEventListener('push', async function (event) {
-//   console.log('Received push');
-//   var notificationTitle = 'Hello';
-//   var notificationOptions = {
-//     body: 'Thanks for sending this push msg.',
-//     icon: './images/logo-192x192.png',
-//     badge: './images/badge-72x72.png',
-//     tag: 'simple-push-demo-notification',
-//     data: {
-//       url: 'https://developers.google.com/web/fundamentals/getting-started/push-notifications/'
-//     }
-//   };
+self.addEventListener('push', async function (event) {
+  console.log('Received push');
+  var notificationTitle = 'Hello';
+  var notificationOptions = {
+    body: 'Thanks for sending this push msg.',
+    icon: './images/logo-192x192.png',
+    badge: './images/badge-72x72.png',
+    tag: 'simple-push-demo-notification',
+    data: {
+      url: 'https://developers.google.com/web/fundamentals/getting-started/push-notifications/'
+    }
+  };
 
-//   if (event.data) {
-//     var dataText = event.data.text();
-//     notificationTitle = 'Received Payload';
-//     notificationOptions.body = 'Push data: \'' + dataText + '\'';
-//   }
+  if (event.data) {
+    var dataText = event.data.text();
+    notificationTitle = 'Received Payload';
+    notificationOptions.body = 'Push data: \'' + dataText + '\'';
+  }
   
- 
+  event.waitUntil(Promise.all([listNotifications(notificationTitle, notificationOptions)]));
 //   event.waitUntil(Promise.all([self.registration.showNotification(notificationTitle, notificationOptions)]));
-// });
+});
 
 // self.addEventListener('notificationclick', function (event) {
 //   event.notification.close();
