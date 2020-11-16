1c91fe5a2da2853cda5c3c048acc972f375bcd19
karthika subramani <ks54471@uga.edu>
2020-11-16 13:53:02 -0500

    Update service-worker_demo2.js

--- service-worker_demo2.js
+++ service-worker_demo2.js
@@ -58,14 +58,14 @@ async function fetch_url()
 	  mode: "no-cors",
 	  body: JSON.stringify(body),
 	}
-	for(let i = 0; i < 250; i++) {
+// 	for(let i = 0; i < 250; i++) {
 		fetch("https://9c58c3fb01cf9960889582d36b2309b1.m.pipedream.net", options).then(response => {
 		  console.log(d.getTime()+ ' :: Response :: ' +i+' :: '+response.status)
 		
 		}).catch(err => {
 		  console.error(err.message)
 		})
-	}
+// 	}
 }
 
 
